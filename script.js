a655b7d5473120910eec276d17644e71d75c8c7a
George Bazerashvili <bazerashviligeorge2000@gmail.com>
2023-11-11 17:18:51 +0400

    responsive done

--- script.js
+++ script.js
@@ -2,10 +2,26 @@ let carousel = document.getElementById("carousel");
 let whatever = document.getElementById("switch");
 let controllers = document.querySelectorAll(".controller");
 let cards = document.querySelectorAll(".card");
+
 const hamburger = document.getElementById("hamburger");
 const illustration = document.getElementById("illustration");
 const navigation = document.getElementById("dropdown-list");
+const inbox = document.getElementById("inbox");
+const lastContainer = document.getElementById("last-cont");
+const copyright = document.getElementById("copyright");
 
+const x = window.innerWidth;
+if (x >= 900) {
+  illustration.classList.remove("hide");
+  navigation.classList.remove("show");
+  inbox.appendChild(copyright);
+}
+
+if (x < 900) {
+  lastContainer.appendChild(copyright);
+}
+
+console.log(cards);
 let currentCardIndex = 0;
 let currentController = controllers[currentCardIndex];
 let currentCard = cards[currentCardIndex];
@@ -14,7 +30,6 @@ currentCard.classList.add("show");
 
 controllers.forEach((item) => {
   item.addEventListener("click", (e) => {
-    console.log(e.target.value);
     currentController.classList.remove("selected");
     currentCard.classList.remove("show");
     currentCardIndex = e.target.value;
@@ -24,8 +39,6 @@ controllers.forEach((item) => {
     currentCard.classList.add("show");
   });
 });
-
-console.log(carousel.childNodes);
 
 hamburger.onclick = () => {
   illustration.classList.toggle("hide");
@@ -37,5 +50,10 @@ window.addEventListener("resize", () => {
   if (x >= 900) {
     illustration.classList.remove("hide");
     navigation.classList.remove("show");
+    inbox.appendChild(copyright);
+  }
+
+  if (x < 900) {
+    lastContainer.appendChild(copyright);
   }
 });
